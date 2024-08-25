dd8c0c0f64a124634cbfd68643542a6deb23ea46
unknown <rohanbadgujar2001@gmail.com>
2024-08-25 11:31:06 +0530

    add dot env

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+
+
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
