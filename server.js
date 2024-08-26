b4ac83a98bbe4beac6a3d9db7aeb772b42405d36
unknown <rohanbadgujar2001@gmail.com>
2024-08-26 11:54:53 +0530

    Mongo db added

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require("express");
 const app = express();
-
-
+const mongoose=require('mongoose')
+const cors=require('cors')
 
 app.use(express.json());
+app.use(cors());
+//connect mongodb
+
+mongoose.connect(process.env.MONGO_URI,{
+
+}).then(()=>console.log("Mongo DB connected")).catch((error)=>console.log(error));
+
+;
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
